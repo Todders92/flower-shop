8e5443d5aa5fcb84a41f56880b572b08a28e27ab
Hristo Dzhambov and Todd Walraven <hristo@>
2020-02-27 16:24:41 -0800

    refactor add price method to correctly total up everything

--- Models/PlantShop.cs
+++ Models/PlantShop.cs
@@ -5,76 +5,77 @@ namespace PlantShop.Models
 {
   public class Bouquet
   {
-    public int Size { get; set; }
+    public string Size { get; set; }
     public string Color { get; set; }
     public int NumberOfFlowers { get; set; }
     public int Price { get; set; }
     
     public Bouquet( string color, int numberOfFlowers, int price)
     {
-      Size = 0;
+      Size = "none";
       Color = color;
       NumberOfFlowers = numberOfFlowers;
       Price = price;
     }
-    public void AddSize(string size)
+    public int AddSize(string size)
     {
       if(size == "small")
       {
-        Size = 1;
+        Size = "small";
+        return 10;
       }
       else if(size == "medium")
        {
-         Size = 2;
+         Size = "medium";
+         return 12;
        }
-      else if (size == "large")
+      else 
       {
-        Size = 3;
+        Size = "large";
+        return 14;
       }
     }
-    public void AddColor(string color)
+    public int AddColor(string color)
     {
       if(color == "red")
       {
         Color = "red";
+        return 1;
       }
       else if (color == "blue")
       {
         Color = "blue";
+        return 2;
       }
       else if (color == "pink")
       {
         Color = "pink";
+        return 1;
       }
       else if (color == "yellow")
       {
         Color = "yellow";
+        return 1;
       }
       else if (color == "purple")
       {
         Color = "purple";
+        return 1;
       }
-      else if (color == "white")
+      else
       {
         Color = "white";
+        return 1;
       }
     }
     public void AddNumber(int number)
     {
     NumberOfFlowers = number;
     }
-    // public void ConvertItems(string size)
-    // {
-    //   if(size == "small")
-    //   {
-    //     size = "10";
-    //     int newsize = int.Parse(size);
-    //     // Console.WriteLine(newsize);
-    //   }
-    // }
-    public void AddPrice(int size , int color, int numberOfFlowers)
+    
+    public void AddPrice(int size , int numberOfFlowers, int color)
     {
-      Price += size + color + numberOfFlowers;
+      Price += (size  + numberOfFlowers) * color;
     }
 
   }
--- Program.cs
+++ Program.cs
@@ -8,22 +8,24 @@ namespace PlantShop
   {
     public static void Main()
     {
-      Bouquet bouqeut = new Bouquet( "0", 0, 10);
+      Bouquet bouqeut = new Bouquet( "0", 0, 0);
       Console.WriteLine("enter the size of vase you would like to buy");
       string response = Console.ReadLine();
-      bouqeut.AddSize(response);
+      int sizeNumber = bouqeut.AddSize(response);
+      Console.WriteLine(sizeNumber);
       Console.WriteLine("bouquet size is " + bouqeut.Size);
       Console.WriteLine("bouquet price is " + bouqeut.Price);
       Console.WriteLine("what color flowers would you like?");
       string colorResponse = Console.ReadLine();
       bouqeut.AddColor(colorResponse);
+      int colorMulitplier = bouqeut.AddColor(colorResponse);
       Console.WriteLine("your flower colors are " + bouqeut.Color);
       Console.WriteLine("How many flowers would you like in your bouquet?");
       string stringNumber = Console.ReadLine();
       int number = int.Parse(stringNumber);
       bouqeut.AddNumber(number);
       Console.WriteLine("the number of flowers in your order is " + bouqeut.NumberOfFlowers);
-      bouqeut.AddPrice(1, number , 1);
+      bouqeut.AddPrice(sizeNumber , number , colorMulitplier);
       Console.WriteLine("your order total is $" + bouqeut.Price);
     }
   }
